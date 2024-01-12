76f50466faa1149a8b5841809ff7f722a9a7b08d
alexander-lh <lasherasalexander@gmail.com>
2024-01-12 21:42:09 +0100

    cors updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     credentials: true,
   })
 );
