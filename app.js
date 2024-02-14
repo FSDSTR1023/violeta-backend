18b19e96f02e8110b355550ed66ca5e1fa341f03
alexander-lh <129536028+alexander-lh@users.noreply.github.com>
2024-02-14 21:02:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://trailnest.netlify.app/',
     credentials: true,
   })
 );
